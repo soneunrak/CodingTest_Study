0c1ae661b6b0fc2a4fa3831e783dd12c3f37d4e6
soneunrak <128948934+soneunrak@users.noreply.github.com>
2026-01-12 16:28:16 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num, int n) {
+        int[] answer = new int[n];
+        
+        for (int i=0; i<n; i++){
+            answer[i] = num[i];
+        }
+        
+        return answer;
+    }
+}
